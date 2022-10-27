aff9df1c3c5e6648b9905aba5d323c6b345bb82c
DanielDiaz4401 <56456407+DanielDiaz4401@users.noreply.github.com>
2022-10-27 13:29:45 +0200

    Used GLib's concurrency manager with random test

--- Puzzle2/app.rb
+++ Puzzle2/app.rb
@@ -1,25 +1,26 @@
-require_relative 'rfid'
+# require_relative 'rfid'
 require 'gtk3'
 require 'thread'
-
+require 'glib2'
 
 class App <Gtk::Window
 
   def initialize
-    @r=Rfid.new
-    @mutex = Mutex.new
-    @cv=ConditionVariable.new
+    @random=Random.new(1234)
+    # @r=Rfid.new
+    @mainloop=GLib::MainLoop.new(nil,true)
     super
 
     set_title "RFID GUI"
     signal_connect "destroy" do
       @thread.kill
-      Gtk.main_quit
+      @mainloop.main_quit
     end
 
     init_ui
 
     show_all
+    @mainloop.run
   end
 
   def init_ui
@@ -28,37 +29,35 @@ class App <Gtk::Window
     @label = Gtk::Label.new 
     @label.text = "Please, login with your university card"
     vbox.pack_start(@label,:expand=>true,:fill=>true)
-    #grid.attach(@label,0,0,1,1)
     @button = Gtk::Button.new "Clear"
     vbox.pack_start(@button,:fill=>true)
-    #grid.attach(@button,0,1,1,1)
     @button.signal_connect "clicked" do
-      @mutex.synchronize{
-        @label.text = "Please, login with your university card"
-        @cv.signal
-      }
+      puts "Netejat"
+      @thread.kill
+      @label.text = "Please, login with your university card"
+      read
     end
-    @thread=Thread.new{
-      @mutex.synchronize{
-        while true do
-          begin
-            read
-          rescue => e
-            puts e.message
-          end
-          @cv.wait(@mutex)
-        end
-      }
-    }
+    
   end
 
   def read
-    puts "hola"
-    uid="uid: #{@r.read_uid}"
-    puts uid
-    @label.text=uid
+    @thread=Thread.new{
+      error=false
+      while error do
+        begin
+          GLib::Idle.add(false,read)
+        rescue => e
+          puts e.message
+          error=true
+        end
+      end
+      # uid="uid: #{@r.read_uid}"
+      sleep(@random.rand(3..5)) # Simulation for the user delay
+      uid="uid: #{@random.rand(1000000.9999999)}"
+      puts "#{uid} llegida"
+      @label.text=uid
+    }
   end
 end
 
 window=App.new
-Gtk.main
