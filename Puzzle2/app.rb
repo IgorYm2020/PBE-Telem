c781f0484eccd6c1e2d5c5462e0f99560fb7c1f7
DanielDiaz4401 <56456407+DanielDiaz4401@users.noreply.github.com>
2022-11-01 17:13:18 +0100

    Added color changes

--- Puzzle2/app.rb
+++ Puzzle2/app.rb
@@ -1,17 +1,25 @@
-require_relative 'rfid'
+#require_relative 'rfid'
 require 'gtk3'
 require 'thread'
 require 'glib2'
 
 class App <Gtk::Window
 
+  @@blue=Gdk::RGBA::new(0,0,1.0,1.0)
+  @@red=Gdk::RGBA::new(1.0,0,0,1.0)
+  @@white=Gdk::RGBA::new(1.0,1.0,1.0,1.0)
+
   def initialize
-    @r=Rfid.new
+    #@r=Rfid.new
+    @random=Random.new(1234)
     @mainloop=GLib::MainLoop.new(nil,true)
     @id=nil
     super
 
     set_title "RFID GUI"
+    set_resizable false
+    set_size_request 400, 100
+    set_border_width 5
     signal_connect "destroy" do
       @mainloop.quit
     end
@@ -19,7 +27,7 @@ class App <Gtk::Window
     init_ui
 
     show_all
-    start_reading
+    Thread.new{read}
     @mainloop.run
   end
 
@@ -28,30 +36,36 @@ class App <Gtk::Window
     add vbox
     @label = Gtk::Label.new 
     @label.text = "Please, login with your university card"
+    @label.override_background_color(:normal,@@blue)
+    @label.override_color(:normal,@@white)
     vbox.pack_start(@label,:expand=>true,:fill=>true)
     @button = Gtk::Button.new :label=>"Clear"
-    vbox.pack_start(@button,:fill=>true)                                                                                             
-    @button.signal_connect "clicked" do                                                                                              
-      if @uid!=nil                                                                                                                   
-        @label.text = "Please, login with your university card"                                                                      
-        @uid=nil                                                                                                                     
-        start_reading                                                                                                                
-      end                                                                                                                            
-    end                                                                                                                              
-  end                                                                                                                                
-                                                                                                                                     
-  def update_uid                                                                                                                     
-    @label.text=@uid                                                                                                                 
-    return false                                                                                                                     
-  end                                                                                                                                
-                                                                                                                                     
-  def read                                                                                                                           
-    uid="uid: #{@r.read_uid}"                                                                                                                                                                                          
-    GLib::Idle.add{update_uid}                                                                                                       
-  end                                                                                                                                
-                                                                                                                                     
-  def start_reading                                                                                                                  
-    Thread.new{read}                                                                                                                 
+    @button.set_focus_on_click true
+    vbox.pack_start(@button,:fill=>true)
+    @button.signal_connect "clicked" do
+      if @uid!=nil
+        @label.text = "Please, login with your university card"
+        @label.override_background_color(:normal,@@blue)
+        @uid=nil
+        Thread.new{read}
+      end
+    end
+  end
+
+  def update_uid
+    @label.text=@uid
+    return false
+  end
+
+  def read
+    sleep(@random.rand(3..5))
+    @uid="uid: #{@random.rand(10000..99999)}"
+    @label.override_background_color(:normal,@@red)
+    #uid="uid: #{@r.read_uid}"
+    GLib::Idle.add{update_uid}
+  end
+
+  def start_reading
   end
 end
 
